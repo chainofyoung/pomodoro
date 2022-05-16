ac6897df1b2a76ae09dd9e30d41f1dacef3d6436
chainofyoung <chainofyoung@gmail.com>
2022-05-17 00:07:05 +0900

    main view add

---
+++ pomodoro/Core/View/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  pomodoro
+//
+//  Created by 최인영 on 2022/05/17.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Main View!")
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
