7a37cbfa79fc23ed0d8e90ae81ef2ebf7995ffcd
chainofyoung <chainofyoung@gmail.com>
2022-05-19 23:58:40 +0900

    Timer 기본 기능 스터디

---
+++ pomodoro/Core/Timer/TimerView.swift
@@ -0,0 +1,35 @@
+//
+//  TimerView.swift
+//  pomodoro
+//
+//  Created by 최인영 on 2022/05/19.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    @State var countDownTimer = 5
+    @State var timerRunning = true
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+   
+    var body: some View {
+        VStack{
+            Text("\(countDownTimer)")
+                .onReceive(timer){ _ in
+                    if countDownTimer > 0 && timerRunning {
+                        countDownTimer -= 1
+                    }else {
+                        timerRunning = false
+                    }
+                }
+                .font(.system(size: 80 , weight: .bold))
+                .opacity(0.80)
+        }
+    }
+}
+
+struct TimerView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimerView()
+    }
+}
